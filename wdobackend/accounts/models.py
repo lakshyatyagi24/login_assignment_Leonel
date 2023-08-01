284df3e530752a86f70caa00bc31dd7d87ccd315
unknown <davidofempire0523@outlook.com>
2023-08-01 08:07:32 -0700

    Add role in user profile.

--- wdobackend/accounts/models.py
+++ wdobackend/accounts/models.py
@@ -28,6 +28,7 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    role = models.CharField(max_length=255)
 
     objects = UserAccountManager()
 
