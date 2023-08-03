ab5e899c70b300fa321a0da2ce7b6c0711f3bb52
George Brown <hero0916@outlook.com>
2023-08-04 00:02:15 +0200

    Change some fields in user account

--- wdobackend/accounts/views.py
+++ wdobackend/accounts/views.py
@@ -5,6 +5,15 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
+from .models import (
+    UserPersonalDetails,
+    UserQualification,
+    UserAddress,
+    IndustryExperience,
+    UserDocuments,
+    UserAccount
+)
+
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def get_current_user(request):
@@ -18,6 +27,29 @@ def get_current_user(request):
     })
 
 @api_view(['POST'])
-@permission_classes([IsAuthenticated])
+@permission_classes([])
 def create_custom_user(request):
-    return JsonResponse("Successfully extracted")
+    data = request.data
+    personal_details = data.get('personal_details')
+    qualification_details = data.get('qualification_details')
+    address = data.get('address')
+    industry_experience = data.get('industry_experience')
+    document_upload = data.get('document_upload')
+
+    personal_details = UserPersonalDetails.from_dict(data['personal_details'])
+    qualification_details = UserQualification.from_dict(data['qualification_details'])
+    address = UserAddress.from_dict(data['address'])
+    document_upload = UserDocuments.from_dict(data['document_upload'])
+
+    user_account = UserAccount(
+        email=personal_details.email_address,
+        role=personal_details.role,
+        personal_details=personal_details,
+        qualification_details=qualification_details,
+        address=address,
+        document_upload=document_upload)
+
+    user_account.save()
+
+    return JsonResponse({"data": "Successfully extracted"})
+
